b8b8cbd2b1831ba61441f24a1e8aebb02ece6b95
Stan <miro_8888@abv.bg>
2022-06-02 17:48:33 +0300

    test for GET and POST Methods

--- QA Homework part1/tests.py
+++ QA Homework part1/tests.py
@@ -6,9 +6,9 @@ def test_api_get():
     for record in resp.json()['category']:
         if record['id'] == 1:
             assert record['name'] == "doggie",\
-                "Data matched! Expected : doggie, found : " + str(record['name'])
+                "Data not matched! Expected : doggie, found : " + str(record['name'])
             assert record['status'] == "pending",\
-                "Data matched! Expected : pending, but found : " + str(record['status'])
+                "Data not matched! Expected : pending, but found : " + str(record['status'])
 
 def test_api_post():
     data = {"bread" : "chihuahua",
@@ -18,7 +18,7 @@ def test_api_post():
     data = resp.json()
     assert (resp.status_code == 201), "Status code is not 201. Rather found : "\
         + str(resp.status_code)
-    assert data['name'] == "Sparky", "Pet created with correct name. \
+    assert data['name'] == "Sparky", "Pet created with wrong name. \
         Expected : Sparky, found : " + str(data['name'])
-    assert data['bread'] == "chihuahua", "pet created with correct bread. \
+    assert data['bread'] == "chihuahua", "pet created with wrong bread. \
         Expected : chihuahua, found : " + str(data['name'])
