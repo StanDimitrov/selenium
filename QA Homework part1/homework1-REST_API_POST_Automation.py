fa2c2a19977cfd9a494c2726f4c65d71ece21018
Stan <miro_8888@abv.bg>
2022-06-02 17:26:37 +0300

    test for GET and POST Methods

--- QA Homework part1/homework1-REST_API_POST_Automation.py
+++ QA Homework part1/homework1-REST_API_POST_Automation.py
@@ -23,7 +23,7 @@ button_avtivation = driver.find_element(By.CLASS_NAME, 'btn try-out__btn').click
 
 #Enter pet characters
 enter_pet_id = driver.find_element(By.CSS_SELECTOR, "#operations-pet-uploadFile > div.no-margin > div > div.opblock-section > div.parameters-container > div > table > tbody > tr:nth-child(1) > td.parameters-col_description > input[type=text]").send_keys("1" + Keys.ENTER)
-enter_additional_data = driver.find_element(By.CSS_SELECTOR, '#operations-pet-uploadFile > div.no-margin > div > div.opblock-section > div.parameters-container > div > table > tbody > tr:nth-child(2) > td.parameters-col_description > input[type=text]').send_keys('{"bread" : "chihjuahua", "name" : "Sparky", "age": "1"}' + Keys.ENTER)
+enter_additional_data = driver.find_element(By.CSS_SELECTOR, '#operations-pet-uploadFile > div.no-margin > div > div.opblock-section > div.parameters-container > div > table > tbody > tr:nth-child(2) > td.parameters-col_description > input[type=text]').send_keys('{"bread" : "chihuahua", "name" : "Sparky", "age": "1"}' + Keys.ENTER)
 upload_photo = driver.find_element(By.XPATH, '#operations-pet-uploadFile > div.no-margin > div > div.opblock-section > div.parameters-container > div > table > tbody > tr:nth-child(3) > td.parameters-col_description > input[type=file]').send_keys('C://Users/Stan/Desktop/esperance-vie-chihuahua.jpg')
 
 
---
+++ QA Homework part1/tests.py
@@ -0,0 +1,24 @@
+import requests
+
+def test_api_get():
+    resp = requests.get("https://petstore.swagger.io/#")
+    assert (resp.status_code == 200), "Status code is 200. : " + str(resp.status_code)
+    for record in resp.json()['category']:
+        if record['id'] == 1:
+            assert record['name'] == "doggie",\
+                "Data matched! Expected : doggie, found : " + str(record['name'])
+            assert record['status'] == "pending",\
+                "Data matched! Expected : pending, but found : " + str(record['status'])
+
+def test_api_post():
+    data = {"bread" : "chihuahua",
+            "name" : "Sparky",
+            "age": "1"}
+    resp = requests.post(url="https://petstore.swagger.io/#/pet/uploadFile", data=data)
+    data = resp.json()
+    assert (resp.status_code == 201), "Status code is not 201. Rather found : "\
+        + str(resp.status_code)
+    assert data['name'] == "Sparky", "Pet created with correct name. \
+        Expected : Sparky, found : " + str(data['name'])
+    assert data['bread'] == "chihuahua", "pet created with correct bread. \
+        Expected : chihuahua, found : " + str(data['name'])
